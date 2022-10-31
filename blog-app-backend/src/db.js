6f68507e472ead3e6daf0605d6cf3c335aea63de
Your Name <you@example.com>
2022-10-31 16:16:17 +0100

    added mongodb to nodejs

---
+++ blog-app-backend/src/db.js
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb';
+
+let db;
+
+async function connectToDatabase(callback){
+    const client=new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+    const DB_NAME = 'react-blog-db';
+    db=client.db(DB_NAME);
+    callback();
+}
+
+export {
+    db,
+    connectToDatabase,
+}
