9907db2f2f4875591368dc1252db4c56e89aabd8
Your Name <you@example.com>
2022-11-02 22:12:54 +0100

    added host mongodb config

--- blog-app-backend/src/db.js
+++ blog-app-backend/src/db.js
@@ -3,7 +3,11 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDatabase(callback) {
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    //local db
+    //const client = new MongoClient('mongodb://127.0.0.1:27017');
+    //mongodb atlas db 
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ugvqpnw.mongodb.net/?retryWrites=true&w=majority`);
+
     await client.connect();
     const DB_NAME = 'react-blog-db';
     db = client.db(DB_NAME);
